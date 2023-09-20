1fed8784410bbe5140803c4dd571e052b75d89da
Christian Hasbani <ch088411@ad.u-bourgogne.fr>
2023-09-20 14:18:49 +0200

    working on ex2

--- TP1_Systemes_Intelligents_avances/src/TP1.java
+++ TP1_Systemes_Intelligents_avances/src/TP1.java
@@ -2,11 +2,12 @@ import jmetal.core.Problem;
 import jmetal.core.Solution;
 import jmetal.core.Variable;
 import jmetal.encodings.solutionType.RealSolutionType;
+import java.lang.Math;
 
 //Exercise 2
-public class Schaffer extends Problem{ // Part 1 create a class that inherets problem
+public class TP1 extends Problem{ // Part 1 create a class that inherets problem
 
-	public Schaffer(String solutionType) { // Part 2 define a constructor 
+	public TP1(String solutionType) { // Part 2 define a constructor 
 		numberOfVariables_ = 30;
 		numberOfObjectives_ = 1;
 		numberOfConstraints_ = 0;
@@ -18,12 +19,13 @@ public class Schaffer extends Problem{ // Part 1 create a class that inherets pr
 			System.out.println("Error: Solution type " + solutionType + " invalid");
 			System.exit(-1);
 		}
+		
 		lowerLimit_ = new double [numberOfVariables_];
 		upperLimit_ = new double [numberOfVariables_];
 		
 		for(int i = 0 ; i < numberOfVariables_; i++) {
-			lowerLimit_[i] = 0;
-			upperLimit_[i] = 1;
+			lowerLimit_[i] = 0.0;
+			upperLimit_[i] = 1.0;
 		}
 	}
 	
@@ -36,12 +38,18 @@ public class Schaffer extends Problem{ // Part 1 create a class that inherets pr
 			x[i] = decisionVariables[i].getValue();
 		}
 		
-		double f1 = 0.0; // Function F1(x)
-		double f2 = 0.0; // Function F2(x)
+		double f1 = x[0]; // Function F1(x)
+		double f2; // Function F2(x)
 		
-		
-		int alpha = 1;
-		int beta = 1;
+//		Calculate the function F2(x)
+		double g = 0;
+		for(int i = 2; i < numberOfVariables_ ; i++) {
+			g += x[i];
+		}
+		g = 1 + 9 * g/(numberOfVariables_-1); 
+		f2 = g * (1- Math.sqrt(f1/g));
+		double alpha = 1.0;
+		double beta = 1.0;
 		double f = alpha*f1+beta*f2; // Function F(x)
 		
 		solution.setObjective(0, f);
